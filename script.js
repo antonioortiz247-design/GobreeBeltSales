7b528ba9f43de8d8e93a7da66dcf72dca294fcc1
antonioortiz247-design <antonioortiz247@gmail.com>
2026-03-04 21:43:34 -0600

    Implement lightbox for gallery images
    Added lightbox functionality for image gallery.

--- script.js
+++ script.js
@@ -36,6 +36,28 @@ lightboxImg.src = img.src
 
 })
 
+const images = document.querySelectorAll(".gallery-img")
+const lightbox = document.getElementById("lightbox")
+const lightboxImg = document.getElementById("lightbox-img")
+const close = document.querySelector(".close")
+
+images.forEach(img => {
+
+img.addEventListener("click", ()=>{
+
+lightbox.style.display = "flex"
+lightboxImg.src = img.src
+
+})
+
+})
+
+close.addEventListener("click", ()=>{
+
+lightbox.style.display = "none"
+
+})
+
 close.addEventListener("click", ()=>{
 
 lightbox.style.display = "none"
